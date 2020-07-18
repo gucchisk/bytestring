30a398286c77f8d8113c050bef326a9d469b18d0
gucchisk <gucchi_sk@yahoo.co.jp>
2020-07-18 22:00:11 +0900

    Refactor

--- bytestring.go
+++ bytestring.go
@@ -3,6 +3,9 @@ package bytestring
 import (
 	"encoding/hex"
 	"encoding/base64"
+	"fmt"
+	"strconv"
+	"strings"
 	"unsafe"
 )
 
@@ -25,54 +28,85 @@ func (b Bytes) HexString() string {
 }
 
 func (b Bytes) Base64() string {
-	return base64.StdEncoding.EncodeToString(b.data)
+	return b.Base64Custom(base64.StdEncoding)
 }
 
-type StringType int
-const (
-	Normal = iota
-	Hex
-	Base64
-)
+func (b Bytes) Base64URL() string {
+	return b.Base64Custom(base64.URLEncoding.WithPadding(base64.NoPadding))
+}
+
+func (b Bytes) Base64Custom(encoding *base64.Encoding) string {
+	return encoding.EncodeToString(b.data)
+}
+
+func (b Bytes) GoByteArray() string {
+	return fmt.Sprintf("%v", b.data)
+}
+
+type Strings interface {
+	toBytes(str string) ([]byte, error)
+}
+
+type NormalString struct {
+}
+
+func (n NormalString) toBytes(str string) ([]byte, error) {
+	return *(*[]byte)(unsafe.Pointer(&str)), nil
+}
+
+type HexString struct {
+}
+
+func (h HexString) toBytes(str string) ([]byte, error) {
+	return hex.DecodeString(str)
+}
+
+type Base64String struct {
+	encoding *base64.Encoding
+}
+
+func (b Base64String) toBytes(str string) ([]byte, error) {
+	return b.encoding.DecodeString(str)
+}
+
+type GoByteArrayString struct {
+}
+
+func (g GoByteArrayString) toBytes(str string) ([]byte, error) {
+	strn := strings.Replace(str, "[", "", 1)
+	strn = strings.Replace(strn, "]", "", 1)
+	strs := strings.Split(strn, " ")
+	bytes := make([]byte, len(strs), len(strs))
+	for i, v := range strs {
+		b, err := strconv.Atoi(v)
+		if err != nil {
+			return bytes, err
+		}
+		if b > 255 || b < 0 {
+			return bytes, fmt.Errorf("bytestring: invalid value of %d at %d", b, i)
+		}
+		bytes[i] = byte(b)
+	}
+	return bytes, nil
+}
+
+var Normal = NormalString{}
+var Hex = HexString{}
+var Base64 = Base64String{base64.StdEncoding}
+var Base64URL = Base64String{base64.URLEncoding.WithPadding(base64.NoPadding)}
+var GoByteArray = GoByteArrayString{}
 
 type String struct {
 	data []byte
 }
 
-func NewString(str string) String {
-	return String{
-		stringToBytes(str),
-	}
-}
-
-func NewStringT(str string, typ StringType) (String, error) {
-	var d []byte
-	var err error = nil
-	switch typ {
-	case Hex:
-		d, err = hexStringToBytes(str)
-	case Base64:
-		d, err = base64StringToBytes(str)
-	default:
-		d = stringToBytes(str)
-	}
+func NewBytesFrom(str string, typ Strings) (String, error) {
+	d, err := typ.toBytes(str)
 	return String{
 		d,
 	}, err
 }
 
-func (s String) Bytes() []byte {
+func (s String) ByteArray() []byte {
 	return s.data
-}
-
-func stringToBytes(str string) []byte {
-	return *(*[]byte)(unsafe.Pointer(&str))
-}
-
-func hexStringToBytes(str string) ([]byte, error) {
-	return hex.DecodeString(str)
-}
-
-func base64StringToBytes(str string) ([]byte, error) {
-	return base64.StdEncoding.DecodeString(str)
 }
--- bytestring_test.go
+++ bytestring_test.go
@@ -1,6 +1,7 @@
 package bytestring
 
 import(
+	"encoding/base64"
 	"reflect"
 	"testing"
 )
@@ -33,33 +34,72 @@ func TestBytesToBase64(t *testing.T) {
 	if str != expectStr {
 		t.Errorf("string not equal (result: %s, expect: %s)", str, expectStr)
 	}
+
+	b = []byte{0xfb, 0xff}
+	bytes = NewBytes(b)
+	str = bytes.Base64()
+	expectStr = "+/8="
+	if str != expectStr {
+		t.Errorf("string not equal (result: %s, expect: %s)", str, expectStr)
+	}
+}
+
+func TestBytesToBase64URL(t *testing.T) {
+	b := []byte{0x68, 0x65, 0x6c, 0x6c, 0x6f}
+	bytes := NewBytes(b)
+	str := bytes.Base64URL()
+	expectStr := "aGVsbG8"
+	if str != expectStr {
+		t.Errorf("string not equal (result: %s, expect: %s)", str, expectStr)
+	}
+
+	b = []byte{0xfb, 0xff}
+	bytes = NewBytes(b)
+	str = bytes.Base64URL()
+	expectStr = "-_8"
+	if str != expectStr {
+		t.Errorf("string not equal (result: %s, expect: %s)", str, expectStr)
+	}
+}
+
+func TestBytesToGoByteArray(t *testing.T) {
+	b := []byte{0x68, 0x65, 0x6c, 0x6c, 0x6f}
+	bytes := NewBytes(b)
+	str := bytes.GoByteArray()
+	expectStr := "[104 101 108 108 111]"
+	if str != expectStr {
+		t.Errorf("string not equal (result: %s, expect: %s)", str, expectStr)
+	}
 }
 
 
 func TestStringToBytes(t *testing.T) {
 	s := "hello"
-	str := NewString(s)
-	bytes := str.Bytes()
+	bytes, err := NewBytesFrom(s, Normal)
+	if err != nil {
+		t.Errorf("error is not nil: %s\n", err)
+	}
+	b := bytes.ByteArray()
 	expectBytes := []byte{0x68, 0x65, 0x6c, 0x6c, 0x6f}
-	if !reflect.DeepEqual(bytes, expectBytes) {
+	if !reflect.DeepEqual(b, expectBytes) {
 		t.Errorf("bytes not equal (result: %d, expect: %d)", bytes, expectBytes)
 	}
 }
 
 func TestHexStringToBytes(t *testing.T) {
 	s := "68656c6c6f"
-	str, err := NewStringT(s, Hex)
+	bytes, err := NewBytesFrom(s, Hex)
 	if err != nil {
 		t.Errorf("error is not nil: %s\n", err)
 	}
-	bytes := str.Bytes()
+	b := bytes.ByteArray()
 	expectBytes := []byte{0x68, 0x65, 0x6c, 0x6c, 0x6f}
-	if !reflect.DeepEqual(bytes, expectBytes) {
+	if !reflect.DeepEqual(b, expectBytes) {
 		t.Errorf("bytes not equal (result: %d, expect: %d)", bytes, expectBytes)
 	}
 
 	s = "abcdefgh"
-	str, err = NewStringT(s, Hex)
+	bytes, err = NewBytesFrom(s, Hex)
 	if err == nil {
 		t.Error("error is nil")
 	}
@@ -69,7 +109,7 @@ func TestHexStringToBytes(t *testing.T) {
 	}
 
 	s = "0abcdef"
-	str, err = NewStringT(s, Hex)
+	bytes, err = NewBytesFrom(s, Hex)
 	if err == nil {
 		t.Error("error is nil")
 	}
@@ -81,43 +121,102 @@ func TestHexStringToBytes(t *testing.T) {
 
 func TestBase64StringToBytes(t *testing.T) {
 	s := "aGVsbG8="
-	str, err := NewStringT(s, Base64)
+	bytes, err := NewBytesFrom(s, Base64)
 	if err != nil {
 		t.Errorf("error is not nil: %s\n", err)
 	}
-	bytes := str.Bytes()
+	b := bytes.ByteArray()
 	expectBytes := []byte{0x68, 0x65, 0x6c, 0x6c, 0x6f}
-	if !reflect.DeepEqual(bytes, expectBytes) {
+	if !reflect.DeepEqual(b, expectBytes) {
 		t.Errorf("bytes not equal (result: %d, expect: %d)", bytes, expectBytes)
 	}
 
-	s = "aGVsbG8"
-	str, err = NewStringT(s, Base64)
+	s = "+/8="
+	bytes, err = NewBytesFrom(s, Base64)
+	if err != nil {
+		t.Errorf("error is not nil: %s\n", err)
+	}
+	b = bytes.ByteArray()
+	expectBytes = []byte{0xfb, 0xff}
+	if !reflect.DeepEqual(b, expectBytes) {
+		t.Errorf("bytes not equal (result: %d, expect: %d)", bytes, expectBytes)
+	}
+}
+
+func TestBase64StringToBytesNoPadding(t *testing.T) {
+	s := "aGVsbG8"
+	bytes, err := NewBytesFrom(s, Base64String{base64.StdEncoding.WithPadding(base64.NoPadding)})
+	b := bytes.ByteArray()
+	if err != nil {
+		t.Error("error is nil")
+	}
+	expectBytes := []byte{0x68, 0x65, 0x6c, 0x6c, 0x6f}
+	if !reflect.DeepEqual(b, expectBytes) {
+		t.Errorf("bytes not equal (result: %d, expect: %d)", bytes, expectBytes)
+	}
+}
+
+func TestBase64StringToBytesWithError(t *testing.T) {
+	s := "abcdefg-"
+	_, err := NewBytesFrom(s, Base64)
 	if err == nil {
 		t.Error("error is nil")
 	}
-	expectErrMsg := "illegal base64 data at input byte 4"
+	expectErrMsg := "illegal base64 data at input byte 7"
+	if err.Error() != expectErrMsg {
+		t.Errorf("error message not eaual (result: %s, expect: %s)\n", err, expectErrMsg)
+	}
+}
+
+
+func TestGoByteArrayStringToBytes(t *testing.T) {
+	s := "[104 101 108 108 111]"
+	bytes, err := NewBytesFrom(s, GoByteArray)
+	if err != nil {
+		t.Errorf("error is not nil: %s\n", err)
+	}
+	b := bytes.ByteArray()
+	expectBytes := []byte{0x68, 0x65, 0x6c, 0x6c, 0x6f}
+	if !reflect.DeepEqual(b, expectBytes) {
+		t.Errorf("bytes not equal (result: %d, expect: %d)", bytes, expectBytes)
+	}
+}
+
+func TestGoByteArrayStringToBytesWithError(t *testing.T) {
+	s := "[104 101 108 108 111 256]"
+	bytes, err := NewBytesFrom(s, GoByteArray)
+	if err == nil {
+		t.Error("error is nil")
+	}
+	_ = bytes.ByteArray()
+	expectErrMsg := "bytestring: invalid value of 256 at 5"
 	if err.Error() != expectErrMsg {
 		t.Errorf("error message not eaual (result: %s, expect: %s)\n", err, expectErrMsg)
 	}
 
-	s = "abcdefg-"
-	str, err = NewStringT(s, Base64)
+	s = "[104 101 108 108 -1]"
+	bytes, err = NewBytesFrom(s, GoByteArray)
 	if err == nil {
 		t.Error("error is nil")
 	}
-	expectErrMsg = "illegal base64 data at input byte 7"
+	_ = bytes.ByteArray()
+	expectErrMsg = "bytestring: invalid value of -1 at 4"
 	if err.Error() != expectErrMsg {
 		t.Errorf("error message not eaual (result: %s, expect: %s)\n", err, expectErrMsg)
 	}
 
-	// s = "0abcdef"
-	// str, err = NewStringT(s, Hex)
-	// if err == nil {
-	// 	t.Error("error is nil")
-	// }
-	// expectErrMsg = "encoding/hex: odd length hex string"
-	// if err.Error() != expectErrMsg {
-	// 	t.Errorf("error message not eaual (result: %s, expect: %s)\n", err, expectErrMsg)
-	// }
+	s = "hello"
+	bytes, err = NewBytesFrom(s, GoByteArray)
+	if err == nil {
+		t.Error("error is nil")
+	}
+	_ = bytes.ByteArray()
+	expectErrMsg = "strconv.Atoi: parsing \"hello\": invalid syntax"
+	if err.Error() != expectErrMsg {
+		t.Errorf("error message not eaual (result: %s, expect: %s)\n", err, expectErrMsg)
+	}
+}
+
+
+func Test(t *testing.T) {
 }
